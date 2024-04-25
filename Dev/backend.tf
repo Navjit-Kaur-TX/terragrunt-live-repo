09d5c3f3ae881133f07cf7c3b3d8007432622282
Navjit-Kaur-TX <132881216+Navjit-Kaur-TX@users.noreply.github.com>
2024-04-25 15:50:41 +0530

    Update backend.tf

--- Dev/backend.tf
+++ Dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
 
 
   backend "s3" {
-    bucket = "demotxstatefile"
+    bucket = "aws-tf-state-bucket-tx"
     region = "us-east-1"
     key = "demotxstatefile.tfstate"
     # ... your Bitbucket backend configuration ...
